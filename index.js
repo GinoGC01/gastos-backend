301637582e39684708f44eaf8e04e725f0b9ee75
Gino <ginociancia10@gmail.com>
2025-07-25 00:12:25 -0300

    Update: PORT for deploy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { PORT } from './src/config.js'
 
 connectDB()
   .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+    app.listen(PORT,"0.0.0.0", () => console.log(`Server running on port ${PORT}`))
     iniciarCronUsuarios()
   })
   .catch(error => {
