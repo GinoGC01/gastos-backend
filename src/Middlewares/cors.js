fb5c7caddfdb75cfff8aeb7874584822e6b6049a
Gino <ginociancia10@gmail.com>
2025-07-25 01:01:53 -0300

    Update: Cors

--- src/Middlewares/cors.js
+++ src/Middlewares/cors.js
@@ -8,15 +8,17 @@ const ACCEPTED_ORIGINS = [
 export const CORS = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
     origin: (origin, callback) => {
-      if (acceptedOrigins.includes(origin)) {
-        return callback(null, true)
-      }
+      if (!origin) return callback(null, true) // permitir desde Postman u otros
 
-      if (!origin) {
-        return callback(null, true)
-      }
+      const cleanOrigin = origin.replace(/\/$/, '') // quita barra final
+      const match = acceptedOrigins.some(o => o === cleanOrigin)
+
+      if (match) return callback(null, true)
 
       return callback(new Error('Not allowed by CORS'))
     },
     credentials: true
   })
+
+  
+export default CORS
